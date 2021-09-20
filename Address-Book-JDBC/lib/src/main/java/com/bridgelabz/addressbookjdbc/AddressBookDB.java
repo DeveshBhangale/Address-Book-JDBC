fb15f9b5262ddd65ce631f7c99933b66f9d6831d
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-20 12:02:54 +0530

    [Devesh]: added feature to retrieve data from address_book table

--- Address-Book-JDBC/lib/src/main/java/com/bridgelabz/addressbookjdbc/AddressBookDB.java
+++ Address-Book-JDBC/lib/src/main/java/com/bridgelabz/addressbookjdbc/AddressBookDB.java
@@ -1,5 +1,51 @@
 package com.bridgelabz.addressbookjdbc;
 
-public class AddressBookDB {
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class AddressBookDB {
+	public Connection connectDb(String dbName, String user,String pass) throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection(String.format("jdbc:mysql://localhost:3306/%s?useSSL=false",dbName),user,pass);
+			return con;
+		}catch(SQLException e){ e.printStackTrace();}
+		catch(ClassNotFoundException e){e.printStackTrace();}
+		return null;
+	}
+	
+	public void getDataFromTable(Connection con) throws SQLException {
+		try{
+			ResultSet rs;
+			DatabaseMetaData dbm = con.getMetaData();
+			ResultSet tables = dbm.getTables(null, null, "address_book", null);
+			if(tables.next()) {
+				String tName = tables.getString("TABLE_NAME");
+				if(tName != null && tName.equals("address_book")) {
+					PreparedStatement stmt=con.prepareStatement("select * from address_book"); 
+					rs=stmt.executeQuery();
+					printData(rs);}
+			}else {
+				Statement stmt=con.createStatement(); 
+				stmt.execute("CREATE TABLE address_book (ID int unsigned not null auto_increment,firstName varchar(10) not null,lastName varchar(20) not null,"
+						+ "address varchar(100) not null,city varchar(10) not null,state varchar(10) not null,zipCode varchar(15) not null,phoneNumber varchar(22) not null,"
+						+ " email varchar(50) not null,primary key(id));");
+				System.out.println("New Table Created");
+		}}catch(SQLException e) {e.printStackTrace();}
+		
+	}
+	
+	public void printData(ResultSet rs) throws SQLException {
+		try {
+			while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)
+							+" "+rs.getString(4)+"  "+rs.getString(5) + " "+rs.getString(6) 
+							+" "+rs.getString(7)+"  "+rs.getString(8) + " "+rs.getString(9) + rs.getString(10));
+			}catch(SQLException e){ e.printStackTrace();}
+	}
 }
--- Address-Book-JDBC/lib/src/test/java/com/bridgelabz/addressbookjdbc/AddressBookDBTest.java
+++ Address-Book-JDBC/lib/src/test/java/com/bridgelabz/addressbookjdbc/AddressBookDBTest.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.addressbookjdbc;
 
-public class AddressBookDBTest {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import org.junit.Test;
+
+public class AddressBookDBTest {
+	public static AddressBookDB addressBookDB = new AddressBookDB();
+	
+	
+	@Test //UC16
+	public void connection() {
+		try{  
+			Connection con = addressBookDB.connectDb("address_book","root","database");
+			con.close();  
+			}catch(Exception e){e.printStackTrace();}  
+	}
+	
+	@Test // UC16
+	public void getDataFromTable() throws Exception {
+		try {
+			Connection con = addressBookDB.connectDb("address_book","root","database");
+			addressBookDB.getDataFromTable(con);
+			con.close();  
+		}catch(SQLException e){e.printStackTrace();} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
